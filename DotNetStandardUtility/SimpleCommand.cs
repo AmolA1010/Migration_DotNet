47bb14bd9fd6b9841c48ff36a4bbca38e1316bf3
Amol Ahire <Amol.Ahire@fdbhealth.com>
2024-02-29 11:02:18 +0530

    checkin

---
+++ DotNetStandardUtility/SimpleCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ShiYing.Utility
+{
+    /**
+     * Simple command that executes with generic parameter.
+     */
+    public class SimpleCommand<T> : CommandBase
+    {
+        public Predicate<T> CanExecuteDelegate { get; set; }
+        public Action<T> ExecuteDelegate { get; set; }
+
+        protected override bool DoCanExecute(object parameter)
+        {
+            return true;
+        }
+
+        protected override void DoExecute(object parameter) => ExecuteDelegate?.Invoke((T)parameter);
+    }
+}
